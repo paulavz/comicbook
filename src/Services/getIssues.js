9db6aeda87db8ee31b06f601c50e4207eace3f07
Paula <paula.sua.008@gmail.com>
2022-04-24 11:07:26 -0400

    Use of backend

--- src/Services/getIssues.js
+++ src/Services/getIssues.js
@@ -1,20 +1,17 @@
 import API from "./Config";
-const TOKEN = process.env.REACT_APP_TOKEN;
+const URLAPI = process.env.REACT_APP_ENDPOINT_API
 
 export const getIssues = async (page = 1, limit = 24) => {
-  const nroPage = (page - 1) * 24;
+  const nroPage = (page - 1)*24;
   try {
-    //   let res = await API.get(`${URLAPI}comic?limit=${limit}&offset=${nroPage}`);
-    let res = await API.get(
-      `https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/issues/?api_key=${TOKEN}&format=json&limit=${limit}&offset=${nroPage}`
-    );
+    let res = await API.get(`${URLAPI}comic?limit=${limit}&offset=${nroPage}`);
     const result = res.data.results.map((d) => ({
       urlImage: d.image.original_url,
       date: d.date_added,
       name: d.volume.name,
       numberIssues: d.issue_number,
-      id: d.id,
-      api_detail_url: d.api_detail_url,
+      id:d.id,
+      api_detail_url:d.api_detail_url,
     }));
     return result;
   } catch (error) {
